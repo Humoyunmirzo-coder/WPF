e02a871a8329786cf86f7d6c5a2275438277911a
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-08-07 14:52:32 +0500

    1

---
+++ Mobile2/Mobile2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Mobile2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Mobile2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
